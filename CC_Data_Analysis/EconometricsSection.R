335e91d2a843d23c0a1ecb6a5d8a6da4fc3ba603
unknown <alishaafici@gmail.com>
2019-11-16 14:25:55 -0500

    Added dataset and non finished cleaning of econometrics

---
+++ CC_Data_Analysis/EconometricsSection.R
@@ -0,0 +1,15 @@
+library(dplyr)
+library(readxl)
+library(janitor)
+library(ggplot2)
+library(tidyr)
+library(stringr)
+library(knitr)
+library(tibble)
+library(purrr)
+library(broom)
+Econometrics <- read_excel("data/WDIEXCEL.xlsx",sheet = "Data")
+Econometrics <- Econometrics %>% clean_names() %>% select(-indicator_code) %>%
+  pivot_longer(cols = starts_with("x"),names_to = "year",values_to = "value") %>%
+  pivot_wider(names_from = indicator_name, values_from = "value") %>%
+  mutate(year = as.numeric(str_remove(year,"x")))
