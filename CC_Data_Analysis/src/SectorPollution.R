128a3691caeb591a312e38b872fdf4308833f0ef
Youssef-Mak <ymakb019@uottawa.ca>
2019-11-17 02:41:17 -0500

    Begin Merging Data

--- CC_Data_Analysis/src/SectorPollution.R
+++ CC_Data_Analysis/src/SectorPollution.R
@@ -1,6 +1,7 @@
 
 library(plyr)
 library(dplyr)
+library(countrycode)
 library(tidyr)
 library(readxl)
 library(janitor)
@@ -8,6 +9,9 @@ library(ggplot2)
 library(skimr)
 library(stringr)
 library(ggrepel)
+
+install.packages("countrycode")
+library(countrycode)
 
 
 country_emissions_filename <- "data/raw/CAIT-Country-CO2-Emissions-Energy-Sub-Sector.csv"
@@ -28,7 +32,8 @@ ce_df <- processCsvData(country_emissions_filename)
 # Per Country Activity Value Added DF
 coun_ava_df <- processCsvData(activity_value_filename) %>% 
   dplyr::rename(activity = subject) %>% 
-  dplyr::rename(year = time)
+  dplyr::rename(year = time) %>% 
+  dplyr::mutate(location = countrycode::countrycode(location, 'iso3c', 'country.name.en'))
 
 coun_ava_df$activity<- plyr::revalue(coun_ava_df$activity, c("AGRFORTFISH" = "Agriculture and Fishery"))
 coun_ava_df$activity<- plyr::revalue(coun_ava_df$activity, c("CONSTR" = "Construction"))
@@ -43,6 +48,14 @@ coun_ava_df$activity<- plyr::revalue(coun_ava_df$activity, c("REALEST" = "Real E
 coun_ava_df$activity<- plyr::revalue(coun_ava_df$activity, c("SERV" = "Services"))
 coun_ava_df$activity<- plyr::revalue(coun_ava_df$activity, c("TOT" = "Total Growth"))
 coun_ava_df$activity<- plyr::revalue(coun_ava_df$activity, c("WHLEHTELTRANSP" = "Wholesale, Retail, Trade, Transport, Accomodation"))
+
+countries_ce <- unique(ce_df$country)
+countries_ce
+countries_df <- unique(coun_ava_df$location)
+countries_df
+
+merged_df <- dplyr::left_join(ce_df, coun_ava_df, by = c("country" = "location", "year" = "year")) 
+  
 
 
 
@@ -62,6 +75,7 @@ int_ava_df %>%
   ) +
   geom_point(size = 2) +
   geom_smooth(se = FALSE, method = lm) +
+  facet_grid(rows = vars(activity)) +
   labs(
     x = "Year",
     y = "Value Added",
